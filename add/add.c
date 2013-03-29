a57788bdf665f5dda17755b9413f84c0d15490d1
User <stepancheg@yandex-team.ru>
2013-03-29 17:13:21 +0400

    test xadd vs lock xadd

---
+++ add/add.c
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+#define E(call) do { if (__builtin_expect(call < 0, 0)) { perror(#call); exit(1); }; } while (false)
+
+long long microseconds() {
+    struct timeval tv;
+    E(gettimeofday(&tv, NULL));
+    return 1000000LL * tv.tv_sec + tv.tv_usec;
+}
+
+#ifndef USE_ATOMIC
+#error define USE_ATOMIC 1 or 0
+#endif
+
+static inline long add(long* a, long b) {
+    __asm__ __volatile__(
+#if USE_ATOMIC
+        "lock\n\t"
+#endif
+        "xadd %0, %1"
+        : "+r" (b), "+m" (a)
+        :
+        : "memory");
+    return *a + b;
+}
+
+int main(int argc, char** argv) {
+    printf("using atomic: %s\n", USE_ATOMIC ? "true" : "false");
+    for (;;) {
+        long long start = microseconds();
+        long long count = 0;
+        while (microseconds() - start < 1000000) {
+            long l = 0;
+            long long batch = 10000000;
+            for (int i = 0; i < batch; ++i) {
+                add(&l, 1);
+            }
+            count += batch;
+        }
+        long long dns = 1000L * (microseconds() - start);
+        long long ns_per_call = dns / count;
+        printf("%lld ns per add\n", ns_per_call);
+    }
+    return 0;
+}
