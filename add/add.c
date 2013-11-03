bc1898cc4168c52857fc619e49a893dc495a20da
Stepan Koltsov <stepan.koltsov@gmail.com>
2013-11-04 02:43:47 +0400

    simple binary for atomic and non-atomic add

--- add/add.c
+++ add/add.c
@@ -12,41 +12,48 @@ long long microseconds() {
     return 1000000LL * tv.tv_sec + tv.tv_usec;
 }
 
-#ifndef USE_ATOMIC
-#error define USE_ATOMIC 1 or 0
-#endif
-
-static inline long add(long* a, long b) {
-    __asm__ __volatile__(
-#if USE_ATOMIC
-        "lock\n\t"
-#endif
-        "xadd %0, %1"
-        : "+r" (b), "+m" (*a)
-        :
-        : "memory");
+static inline long add(long* a, long b, bool use_atomic) {
+    if (use_atomic) {
+        __asm__ __volatile__(
+            "lock\n\t"
+            "xadd %0, %1"
+            : "+r" (b), "+m" (*a)
+            :
+            : "memory");
+    } else {
+        __asm__ __volatile__(
+            "xadd %0, %1"
+            : "+r" (b), "+m" (*a)
+            :
+            : "memory");
+    }
     return *a + b;
 }
 
-int main(int argc, char** argv) {
-    printf("using atomic: %s\n", USE_ATOMIC ? "true" : "false");
-    for (;;) {
-        long long start = microseconds();
-        long long count = 0;
-        while (microseconds() - start < 1000000) {
-            long l = 0;
-            long long batch = 10000000;
-            for (int i = 0; i < batch; ++i) {
-                add(&l, 1);
-            }
-            if (__builtin_expect(l != batch, 0)) {
-                abort();
-            }
-            count += batch;
+static void run_test(bool use_atomic) {
+    long long start = microseconds();
+    long long count = 0;
+    while (microseconds() - start < 1000000) {
+        long l = 0;
+        long long batch = 10000000;
+        for (int i = 0; i < batch; ++i) {
+            add(&l, 1, use_atomic);
         }
-        long long dps = 1000000L * (microseconds() - start);
-        long long ps_per_call = dps / count;
-        printf("%lld ps per add\n", ps_per_call);
+        if (__builtin_expect(l != batch, 0)) {
+            abort();
+        }
+        count += batch;
+    }
+    long long dps = 1000000L * (microseconds() - start);
+    long long ps_per_call = dps / count;
+    printf("using atomic: %s\n", use_atomic ? "true" : "false");
+    printf("%lld ps per add\n", ps_per_call);
+}
+
+int main(int argc, char** argv) {
+    for (;;) {
+        run_test(false);
+        run_test(true);
     }
     return 0;
 }
