ae25db791576d78b0b3cbabb7410a35aa6b39d94
Stepan Koltsov <stepan.koltsov@gmail.com>
2013-11-04 02:52:45 +0400

    test int and long

--- add/add.c
+++ add/add.c
@@ -12,7 +12,7 @@ long long microseconds() {
     return 1000000LL * tv.tv_sec + tv.tv_usec;
 }
 
-static inline long add(long* a, long b, bool use_atomic) {
+static inline long addl(long* a, long b, bool use_atomic) {
     if (use_atomic) {
         __asm__ __volatile__(
             "lock\n\t"
@@ -30,30 +30,57 @@ static inline long add(long* a, long b, bool use_atomic) {
     return *a + b;
 }
 
-static void run_test(bool use_atomic) {
+static inline int addi(int* a, int b, bool use_atomic) {
+    if (use_atomic) {
+        __asm__ __volatile__(
+            "lock\n\t"
+            "xadd %0, %1"
+            : "+r" (b), "+m" (*a)
+            :
+            : "memory");
+    } else {
+        __asm__ __volatile__(
+            "xadd %0, %1"
+            : "+r" (b), "+m" (*a)
+            :
+            : "memory");
+    }
+    return *a + b;
+}
+
+static void run_test(bool use_atomic, bool long_or_int) {
     long long start = microseconds();
     long long count = 0;
     while (microseconds() - start < 1000000) {
         long l = 0;
+        int i = 0;
         long long batch = 10000000;
-        for (int i = 0; i < batch; ++i) {
-            add(&l, 1, use_atomic);
+        for (int k = 0; k < batch; ++k) {
+            if (long_or_int) {
+                addl(&l, 1, use_atomic);
+            } else {
+                addi(&i, 1, use_atomic);
+            }
         }
-        if (__builtin_expect(l != batch, 0)) {
+        if (__builtin_expect((l + i) != batch, 0)) {
             abort();
         }
         count += batch;
     }
     long long dps = 1000000L * (microseconds() - start);
     long long ps_per_call = dps / count;
-    printf("using atomic: %s\n", use_atomic ? "true" : "false");
+    printf("using atomic: %s, base type: %s\n",
+        use_atomic ? "true" : "false",
+        long_or_int ? "long" : "int");
     printf("%lld ps per add\n", ps_per_call);
 }
 
 int main(int argc, char** argv) {
     for (;;) {
-        run_test(false);
-        run_test(true);
+        run_test(false, true);
+        run_test(false, false);
+        run_test(true, true);
+        run_test(true, false);
     }
     return 0;
 }
